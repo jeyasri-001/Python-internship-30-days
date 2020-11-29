098151c812ca221cb210d6c85030bb2b40c587d6
jeyasri__001 <56224991+jeyasri-001@users.noreply.github.com>
2020-11-29 11:44:28 +0530

    Add files via upload

---
+++ Day 5/task1.py
@@ -0,0 +1,8 @@
+def func(a,b):
+    print("Addition %d"%(a+b))
+    print("Subtraction %d"%(a-b))
+    print("Division  %d"%(a/b))
+    print("Multiplication %d"%(a*b))
+
+
+func(a=8,b=10)
---
+++ Day 5/task2.py
@@ -0,0 +1,8 @@
+def covid(patient_name,body_temp=98):
+    print("Patient name is ",patient_name)
+    print("\nBody temperature = ",body_temp,"°C")
+    print("---------------------------------")
+
+covid("Rajesh",101)
+covid("Kumar",100)
+covid("ramesh")
